0a989473898bd4ff7610f953db19ca8da9d0ccb5
Felix Ojiambo <felixojiamboe@gmail.com>
2024-06-12 08:34:33 -0700

    Implemented CustomerService with CRUD operations, including error handling for non-existent customers.

--- microservices/customer/src/main/java/com/ecom/customer/CustomerApplication.java
+++ microservices/customer/src/main/java/com/ecom/customer/CustomerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CustomerApplication.class, args);
+		System.out.println("Customer Service");
 	}
 
 }
---
+++ microservices/customer/src/main/java/com/ecom/customer/handler/ErrorResponse.java
@@ -0,0 +1,9 @@
+package com.ecom.customer.handler;
+import java.util.Map;
+import java.util.Set;
+
+public record ErrorResponse(
+        Map<String, String> errors
+) {
+
+}
