2ddad32effa43522cfe11291ebd7bf757eca1611
Felix Ojiambo <felixojiamboe@gmail.com>
2024-06-12 09:38:18 -0700

    Implemented ProductService with product creation, retrieval, and bulk purchase functionality, including transaction management for purchase operations.

---
+++ microservices/product/src/main/java/com/ecom/product/product/Product.java
@@ -0,0 +1,30 @@
+package com.ecom.product.product;
+
+import com.ecom.product.category.Category;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    private double availableQuantity;
+    private BigDecimal price;
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
